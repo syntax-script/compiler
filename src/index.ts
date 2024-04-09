8d6168d005ef817993262fb64c7cfd0e683e38c6
efekos <hefeolgun10@gmail.com>
2024-04-10 01:17:22 +0300

    export everything manually

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-export * from './ast.js';
-export * from './compiler.js';
-export * from './lexer.js';
-export * from './types.js';
-export * from './diagnostic.js';
-export * from './diagnosticTypes.js';
+import { AnyExportable, Export, ExportType, Function, Keyword, OneParameterMethod, Operator, ReturnerMethod, SyntaxScriptCompiler, escapeRegex } from './compiler.js';
+import { BraceExpression, CompileStatement, ExportStatement, Expression, FunctionStatement, ImportStatement, ImportsStatement, KeywordStatement, OperatorStatement, ParenExpression, PrimitiveTypeExpression, ProgramStatement, RuleStatement, SquareExpression, Statement, StringExpression, VariableExpression, WhitespaceIdentifierExpression } from './types.js';
+import { CodeDescription, Diagnostic, DiagnosticRelatedInformation, DiagnosticSeverity, DiagnosticTag, DocumentDiagnosticReportKind, DocumentUri, FullDocumentDiagnosticReport, Location, Position, Range, URI } from './diagnosticTypes.js';
+import { CompilerError, isCompilerError } from './types.js';
+import { Node, NodeType, Token, TokenType } from './types.js';
+import { SyxConfig, SyxConfigCompile } from './types.js';
+import { sysparser, syxparser } from './ast.js';
+import { tokenizeSys, tokenizeSyx } from './lexer.js';
+import { createSyntaxScriptDiagnosticReport } from './diagnostic.js';
+
+
+export { sysparser, syxparser };
+export { escapeRegex, createSyntaxScriptDiagnosticReport, tokenizeSys, tokenizeSyx, isCompilerError };
+
+
+export { SyntaxScriptCompiler, ExportType };
+export { AnyExportable, Export, Function, Keyword, OneParameterMethod, Operator, ReturnerMethod };
+export { Diagnostic, FullDocumentDiagnosticReport, CodeDescription, DiagnosticRelatedInformation, DiagnosticSeverity, DiagnosticTag, DocumentDiagnosticReportKind, DocumentUri, Location, Position, Range, URI };
+
+export { CompilerError };
+export { Token, TokenType, Node, NodeType };
+export { Expression, BraceExpression, ParenExpression, SquareExpression, StringExpression, VariableExpression, PrimitiveTypeExpression, WhitespaceIdentifierExpression };
+export { Statement, RuleStatement, ExportStatement, ImportStatement, ImportsStatement, CompileStatement, OperatorStatement, FunctionStatement, KeywordStatement, ProgramStatement };
+export { SyxConfig, SyxConfigCompile };
