5ec4540dcb336e4c5f62c830dd48fdee5ca1f398
efekos <hefeolgun10@gmail.com>
2024-04-09 17:41:10 +0300

    add diagnostic namespace

---
+++ src/diagnostic.ts
@@ -0,0 +1,38 @@
+import { DiagnosticSeverity, DocumentDiagnosticReportKind, FullDocumentDiagnosticReport } from './diagnosticTypes.js';
+import { sysparser, syxparser } from './ast.js';
+import { tokenizeSys, tokenizeSyx } from './lexer.js';
+import { CompilerError } from './types.js';
+import { readFileSync } from 'fs';
+
+
+export namespace diagnostic {
+
+    /**
+     * Creates a diagnostic report from the file path given.
+     * @param {string} filePath Path of the file to create a report. 
+     * @returns A diagnostic report language servers can use.
+     */
+    export function createReport(filePath: string): FullDocumentDiagnosticReport {
+        const isSyx: boolean = filePath.endsWith('.syx');
+
+        try {
+
+            const content = readFileSync(filePath).toString();
+            const tokens = (isSyx ? tokenizeSyx : tokenizeSys)(content);
+            (isSyx ? syxparser : sysparser).parseTokens(tokens);
+
+            return { kind: DocumentDiagnosticReportKind.Full, items: [] };
+        } catch (error) {
+            if (error instanceof CompilerError) {
+                const compilerError = error as CompilerError;
+                return {
+                    kind: DocumentDiagnosticReportKind.Full, items: [
+                        { message: compilerError.message, range: compilerError.range, severity: DiagnosticSeverity.Error, source: 'syntax-script' }
+                    ]
+                };
+            }
+        }
+
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './ast.js';
 export * from './compiler.js';
 export * from './lexer.js';
 export * from './types.js';
+export * from './diagnostic.js';
+export * from './diagnosticTypes.js';
