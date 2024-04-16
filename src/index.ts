b23a68dedb1bdef717b29ee076bc0f3ddc06ad18
efekos <hefeolgun10@gmail.com>
2024-04-16 08:04:40 +0300

    remove ExportStatement from exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { AnyExportable, Export, ExportType, Function, Keyword, OneParameterMethod, Operator, ReturnerMethod, SyntaxScriptCompiler, escapeRegex } from './compiler.js';
 import { BaseRule, BooleanRule, Functionary, FunctionaryValueType, Rule, RuleType, StringRule, dictionary } from './dictionary/index.js';
-import { BraceExpression, CompileStatement, ExportStatement, Expression, FunctionStatement, ImportStatement, ImportsStatement, KeywordStatement, OperatorStatement, ParenExpression, PrimitiveTypeExpression, ProgramStatement, RuleStatement, SquareExpression, Statement, StringExpression, VariableExpression, WhitespaceIdentifierExpression } from './types.js';
+import { BraceExpression, CompileStatement, Expression, FunctionStatement, ImportStatement, ImportsStatement, KeywordStatement, OperatorStatement, ParenExpression, PrimitiveTypeExpression, ProgramStatement, RuleStatement, SquareExpression, Statement, StringExpression, VariableExpression, WhitespaceIdentifierExpression } from './types.js';
 import { CompilerError, isCompilerError } from './types.js';
 import { Node, NodeType, Token, TokenType } from './types.js';
 import { SyxConfig, SyxConfigCompile } from './types.js';
@@ -22,5 +22,5 @@ export { AnyExportable, Export, Function, Keyword, OneParameterMethod, Operator,
 export { CompilerError };
 export { Token, TokenType, Node, NodeType };
 export { Expression, BraceExpression, ParenExpression, SquareExpression, StringExpression, VariableExpression, PrimitiveTypeExpression, WhitespaceIdentifierExpression };
-export { Statement, RuleStatement, ExportStatement, ImportStatement, ImportsStatement, CompileStatement, OperatorStatement, FunctionStatement, KeywordStatement, ProgramStatement };
+export { Statement, RuleStatement, ImportStatement, ImportsStatement, CompileStatement, OperatorStatement, FunctionStatement, KeywordStatement, ProgramStatement };
 export { SyxConfig, SyxConfigCompile };
