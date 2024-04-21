0dc90e4a2e16d7de58f706e2d415e1fbd1791136
efekos <hefeolgun10@gmail.com>
2024-04-21 17:15:28 +0300

    update 'Compiler module should provide correct ranges' test with 3 different cases

--- src/test/compiler.test.ts
+++ src/test/compiler.test.ts
@@ -1,6 +1,6 @@
 import { CompileStatement, FunctionStatement, GlobalStatement, ImportStatement, ImportsStatement, KeywordStatement, NodeType, ProgramStatement, RuleStatement, TokenType, isCompilerError } from '../types.js';
 import { Diagnostic, DiagnosticSeverity, DocumentDiagnosticReportKind, Range } from 'lsp-types';
-import { describe, it, onError } from '@efekos/es-test/bin/testRunner.js';
+import { describe, inst, it, onError } from '@efekos/es-test/bin/testRunner.js';
 import { tokenizeSys, tokenizeSyx } from '../lexer.js';
 import { createSyntaxScriptDiagnosticReport } from '../diagnostic.js';
 import { expect } from 'chai';
@@ -19,21 +19,64 @@ describe('Compiler module', () => {
 
     it('should provide correct ranges', () => {
 
-        const tokens = tokenizeSyx('keyword hello;');
+        inst(() => {
 
-        rangeExpectations(tokens[0].range);
-        expect(tokens[0].range).to.deep.equal({ end: { line: 1, character: 8 }, start: { line: 1, character: 1 } });
-        expect(tokens[1].range).to.deep.equal({ end: { line: 1, character: 14 }, start: { line: 1, character: 9 } });
-        expect(tokens[2].range).to.deep.equal({ end: { line: 1, character: 15 }, start: { line: 1, character: 14 } });
+            const tokens = tokenizeSyx('keyword hello;');
 
-    });
+            tokens.map(r=>r.range).forEach(r=>rangeExpectations(r));
+            expect(tokens[0].range).to.deep.equal({ end: { line: 1, character: 8 }, start: { line: 1, character: 1 } });
+            expect(tokens[1].range).to.deep.equal({ end: { line: 1, character: 14 }, start: { line: 1, character: 9 } });
+            expect(tokens[2].range).to.deep.equal({ end: { line: 1, character: 15 }, start: { line: 1, character: 14 } });
+
+        });
+
+        inst(() => {
+
+            const tokens = tokenizeSyx('rule "imports-keyword": cray;');
+
+            expect(tokens).to.be.a('array').to.have.lengthOf(10);
+            tokens.map(r=>r.range).forEach(r=>rangeExpectations(r));
+            expect(tokens[0].range).to.be.deep.equal({ end: { line: 1, character: 5 }, start: { line: 1, character: 1 } });
+            expect(tokens[1].range).to.be.deep.equal({ end: { line: 1, character: 7 }, start: { line: 1, character: 6 } });
+            expect(tokens[2].range).to.be.deep.equal({ end: { line: 1, character: 14 }, start: { line: 1, character: 7 } });
+            expect(tokens[3].range).to.be.deep.equal({ end: { line: 1, character: 15 }, start: { line: 1, character: 14 } });
+            expect(tokens[4].range).to.be.deep.equal({ end: { line: 1, character: 22 }, start: { line: 1, character: 15 } });
+            expect(tokens[5].range).to.be.deep.equal({ end: { line: 1, character: 23 }, start: { line: 1, character: 22 } });
+            expect(tokens[6].range).to.be.deep.equal({ end: { line: 1, character: 24 }, start: { line: 1, character: 23 } });
+            expect(tokens[7].range).to.be.deep.equal({ end: { line: 1, character: 29 }, start: { line: 1, character: 25 } });
+            expect(tokens[8].range).to.be.deep.equal({ end: { line: 1, character: 30 }, start: { line: 1, character: 29 } });
+
+        });
+
+        inst(()=>{
+            const tokens = tokenizeSyx('rule "return-function-value-enabled":true;');
+
+            expect(tokens).to.be.a('array').to.have.lengthOf(14);
+            tokens.map(r=>r.range).forEach(r=>rangeExpectations(r));
+            expect(tokens[0].range).to.be.deep.equal({ end: { line: 1, character: 5 }, start: { line: 1, character: 1 } });
+            expect(tokens[1].range).to.be.deep.equal({ end: { line: 1, character: 7 }, start: { line: 1, character: 6 } });
+            expect(tokens[2].range).to.be.deep.equal({ end: { line: 1, character: 13 }, start: { line: 1, character: 7 } });
+            expect(tokens[3].range).to.be.deep.equal({ end: { line: 1, character: 14 }, start: { line: 1, character: 13 } });
+            expect(tokens[4].range).to.be.deep.equal({ end: { line: 1, character: 22 }, start: { line: 1, character: 14 } });
+            expect(tokens[5].range).to.be.deep.equal({ end: { line: 1, character: 23 }, start: { line: 1, character: 22 } });
+            expect(tokens[6].range).to.be.deep.equal({ end: { line: 1, character: 28 }, start: { line: 1, character: 23 } });
+            expect(tokens[7].range).to.be.deep.equal({ end: { line: 1, character: 29 }, start: { line: 1, character: 28 } });
+            expect(tokens[8].range).to.be.deep.equal({ end: { line: 1, character: 36 }, start: { line: 1, character: 29 } });
+            expect(tokens[9].range).to.be.deep.equal({ end: { line: 1, character: 37 }, start: { line: 1, character: 36 } });
+            expect(tokens[10].range).to.be.deep.equal({ end: { line: 1, character: 38 }, start: { line: 1, character: 37 } });
+            expect(tokens[11].range).to.be.deep.equal({ end: { line: 1, character: 42 }, start: { line: 1, character: 38 } });
+            expect(tokens[12].range).to.be.deep.equal({ end: { line: 1, character: 43 }, start: { line: 1, character: 42 } });
+
+        });
+
+    }, true);
 
     it('should provide correct tokenization', () => {
         const t = tokenizeSyx('class } > ) ] , compile "" export function global random import imports 1 keyword { < ( [ operator * rule ; \'\' | +s');
         const tList = [
-            TokenType.ClassKeyword, TokenType.CloseBrace, TokenType.CloseDiamond, TokenType.CloseParen, TokenType.CloseSquare, TokenType.Comma, TokenType.CompileKeyword, TokenType.DoubleQuote,TokenType.DoubleQuote,
+            TokenType.ClassKeyword, TokenType.CloseBrace, TokenType.CloseDiamond, TokenType.CloseParen, TokenType.CloseSquare, TokenType.Comma, TokenType.CompileKeyword, TokenType.DoubleQuote, TokenType.DoubleQuote,
             TokenType.ExportKeyword, TokenType.FunctionKeyword, TokenType.GlobalKeyword, TokenType.Identifier, TokenType.ImportKeyword, TokenType.ImportsKeyword, TokenType.IntNumber, TokenType.KeywordKeyword,
-            TokenType.OpenBrace, TokenType.OpenDiamond, TokenType.OpenParen, TokenType.OpenSquare, TokenType.OperatorKeyword, TokenType.Raw, TokenType.RuleKeyword, TokenType.Semicolon, TokenType.SingleQuote,TokenType.SingleQuote,
+            TokenType.OpenBrace, TokenType.OpenDiamond, TokenType.OpenParen, TokenType.OpenSquare, TokenType.OperatorKeyword, TokenType.Raw, TokenType.RuleKeyword, TokenType.Semicolon, TokenType.SingleQuote, TokenType.SingleQuote,
             TokenType.VarSeperator, TokenType.WhitespaceIdentifier, TokenType.EndOfFile
         ];
 
@@ -41,7 +84,7 @@ describe('Compiler module', () => {
         expect(t.map(tt => tt.type)).to.be.deep.equal(tList);
 
         const sys = tokenizeSys('import "" \'\' ; :::');
-        const sysList = [TokenType.ImportKeyword, TokenType.DoubleQuote,TokenType.DoubleQuote, TokenType.SingleQuote,TokenType.SingleQuote, TokenType.Semicolon, TokenType.EndOfFile];
+        const sysList = [TokenType.ImportKeyword, TokenType.DoubleQuote, TokenType.DoubleQuote, TokenType.SingleQuote, TokenType.SingleQuote, TokenType.Semicolon, TokenType.EndOfFile];
 
         expect(sys).to.be.a('array');
         expect(sys.map(tt => tt.type)).to.be.deep.equal(sysList);
@@ -139,7 +182,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('export keyword ruleish;');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: KeywordStatement = { type: NodeType.Keyword, modifiers: [{range:{end:{line:1,character:7},start:{line:1,character:1}},type:TokenType.ExportKeyword,value:'export'}], range: { end: { line: 1, character: 23 }, start: { line: 1, character: 1 } }, word: 'ruleish' };
+            const stmt: KeywordStatement = { type: NodeType.Keyword, modifiers: [{ range: { end: { line: 1, character: 7 }, start: { line: 1, character: 1 } }, type: TokenType.ExportKeyword, value: 'export' }], range: { end: { line: 1, character: 23 }, start: { line: 1, character: 1 } }, word: 'ruleish' };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -148,16 +191,16 @@ describe('Compiler module', () => {
 
     });
 
-    it('should provide correct diagnostic reports',()=>{
+    it('should provide correct diagnostic reports', () => {
 
-        const report = createSyntaxScriptDiagnosticReport('TEST_FILE.syx','keyword ruleis');
+        const report = createSyntaxScriptDiagnosticReport('TEST_FILE.syx', 'keyword ruleis');
 
         expect(report).to.be.a('object');
         expect(report).to.have.property('items').to.be.a('array').to.have.lengthOf(1);
         expect(report).to.have.property('kind').to.be.a('string').to.be.equal(DocumentDiagnosticReportKind.Full);
 
         const diag = report.items[0];
-        const item: Diagnostic = {message:'Expected \';\' after statement, found \'EOF\'.',range:{start:{line:0,character:0},end:{line:0,character:0}},severity:DiagnosticSeverity.Error,source:'syntax-script',data:[]};
+        const item: Diagnostic = { message: 'Expected \';\' after statement, found \'EOF\'.', range: { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } }, severity: DiagnosticSeverity.Error, source: 'syntax-script', data: [] };
 
         expect(diag).to.have.property('message').to.be.a('string');
         expect(diag).to.have.property('range');
