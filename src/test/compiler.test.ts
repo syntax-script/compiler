702858c76f1c97b7cefe589202ed235a5b6c1630
efekos <hefeolgun10@gmail.com>
2024-04-23 03:02:16 +0300

    create two cases for diagnostic reports (one failed)

--- src/test/compiler.test.ts
+++ src/test/compiler.test.ts
@@ -1,5 +1,5 @@
+import { CodeAction, CodeActionKind, Diagnostic, DiagnosticSeverity, DocumentDiagnosticReportKind, FullDocumentDiagnosticReport, Range } from 'lsp-types';
 import { CompileStatement, FunctionStatement, GlobalStatement, ImportStatement, ImportsStatement, KeywordStatement, NodeType, ProgramStatement, RuleStatement, Token, TokenType, isCompilerError } from '../types.js';
-import { Diagnostic, DiagnosticSeverity, DocumentDiagnosticReportKind, Range } from 'lsp-types';
 import { describe, inst, it } from '@efekos/es-test/bin/testRunner.js';
 import { tokenizeSys, tokenizeSyx } from '../lexer.js';
 import { HandlerFn } from '@efekos/es-test/bin/types.js';
@@ -7,9 +7,15 @@ import { createSyntaxScriptDiagnosticReport } from '../diagnostic.js';
 import { expect } from 'chai';
 import { syxparser } from '../ast.js';
 
-function r(sc:number,ec:number):Range {
-    return {start:{line:1,character:sc},end:{line:1,character:ec}};
+function r(sc: number, ec: number): Range {
+    return { start: { line: 1, character: sc }, end: { line: 1, character: ec } };
 }
+
+
+function r0(sc: number, ec: number): Range {
+    return { start: { line: 0, character: sc }, end: { line: 0, character: ec } };
+}
+
 
 describe('Compiler module', () => {
 
@@ -204,7 +210,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('keyword ruleish;');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: KeywordStatement = { type: NodeType.Keyword, modifiers: [], range: { end: { line: 1, character: 16 }, start: { line: 1, character: 1 } }, word: {value:'ruleish',type:NodeType.Identifier,modifiers:[],range:{start:{line:1,character:9},end:{line:1,character:16}}} };
+            const stmt: KeywordStatement = { type: NodeType.Keyword, modifiers: [], range: { end: { line: 1, character: 16 }, start: { line: 1, character: 1 } }, word: { value: 'ruleish', type: NodeType.Identifier, modifiers: [], range: { start: { line: 1, character: 9 }, end: { line: 1, character: 16 } } } };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -215,7 +221,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('rule \'function-value-return-enabled\': true;');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: RuleStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 43 } }, modifiers: [], rule: {range:r(6,37),type:NodeType.String,value:'function-value-return-enabled',modifiers:[]}, value: 'true', type: NodeType.Rule };
+            const stmt: RuleStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 43 } }, modifiers: [], rule: { range: r(6, 37), type: NodeType.String, value: 'function-value-return-enabled', modifiers: [] }, value: 'true', type: NodeType.Rule };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -226,7 +232,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('compile(ts,js) \'test\';');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: CompileStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 22 } }, formats: [{modifiers:[],type:NodeType.Identifier,range:r(9,11),value:'ts'},{modifiers:[],type:NodeType.Identifier,range:r(12,14),value:'js'}], type: NodeType.Compile, modifiers: [], body: [{ type: NodeType.String, modifiers: [], range: { start: { line: 1, character: 16 }, end: { line: 1, character: 22 } }, value: 'test' }] };
+            const stmt: CompileStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 22 } }, formats: [{ modifiers: [], type: NodeType.Identifier, range: r(9, 11), value: 'ts' }, { modifiers: [], type: NodeType.Identifier, range: r(12, 14), value: 'js' }], type: NodeType.Compile, modifiers: [], body: [{ type: NodeType.String, modifiers: [], range: { start: { line: 1, character: 16 }, end: { line: 1, character: 22 } }, value: 'test' }] };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -237,7 +243,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('imports(ts,js) \'math\';');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: ImportsStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 22 } }, formats: [{modifiers:[],type:NodeType.Identifier,range:r(9,11),value:'ts'},{modifiers:[],type:NodeType.Identifier,range:r(12,14),value:'js'}], type: NodeType.Imports, modifiers: [], module: {range:r(16,22),modifiers:[],type:NodeType.String,value:'math'} };
+            const stmt: ImportsStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 22 } }, formats: [{ modifiers: [], type: NodeType.Identifier, range: r(9, 11), value: 'ts' }, { modifiers: [], type: NodeType.Identifier, range: r(12, 14), value: 'js' }], type: NodeType.Imports, modifiers: [], module: { range: r(16, 22), modifiers: [], type: NodeType.String, value: 'math' } };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -248,7 +254,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('global randomizer {}');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: GlobalStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 21 } }, name: {type:NodeType.Identifier,modifiers:[],range:r(8,18),value:'randomizer'}, type: NodeType.Global, modifiers: [], body: [] };
+            const stmt: GlobalStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 21 } }, name: { type: NodeType.Identifier, modifiers: [], range: r(8, 18), value: 'randomizer' }, type: NodeType.Global, modifiers: [], body: [] };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -259,7 +265,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('function randomizer <int> {}');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: FunctionStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 29 } }, name: {type:NodeType.Identifier,modifiers:[],range:r(10,20),value:'randomizer'}, type: NodeType.Function, modifiers: [], body: [], arguments: [{modifiers:[],range:r(21,26),type:NodeType.PrimitiveType,value:'int'}] };
+            const stmt: FunctionStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 29 } }, name: { type: NodeType.Identifier, modifiers: [], range: r(10, 20), value: 'randomizer' }, type: NodeType.Function, modifiers: [], body: [], arguments: [{ modifiers: [], range: r(21, 26), type: NodeType.PrimitiveType, value: 'int' }] };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -270,7 +276,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('import \'./math\';');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: ImportStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 16 } }, type: NodeType.Import, modifiers: [], path: {range:r(8,16),value:'./math',modifiers:[],type:NodeType.String} };
+            const stmt: ImportStatement = { range: { start: { line: 1, character: 1 }, end: { line: 1, character: 16 } }, type: NodeType.Import, modifiers: [], path: { range: r(8, 16), value: './math', modifiers: [], type: NodeType.String } };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -281,7 +287,7 @@ describe('Compiler module', () => {
 
             const tokens = tokenizeSyx('export keyword ruleish;');
             const ast = syxparser.parseTokens(tokens, 'TEST_FILE');
-            const stmt: KeywordStatement = { type: NodeType.Keyword, modifiers: [{ range: { end: { line: 1, character: 7 }, start: { line: 1, character: 1 } }, type: TokenType.ExportKeyword, value: 'export' }], range: { end: { line: 1, character: 23 }, start: { line: 1, character: 1 } }, word: {range:r(16,23),modifiers:[],type:NodeType.Identifier,value:'ruleish'} };
+            const stmt: KeywordStatement = { type: NodeType.Keyword, modifiers: [{ range: { end: { line: 1, character: 7 }, start: { line: 1, character: 1 } }, type: TokenType.ExportKeyword, value: 'export' }], range: { end: { line: 1, character: 23 }, start: { line: 1, character: 1 } }, word: { range: r(16, 23), modifiers: [], type: NodeType.Identifier, value: 'ruleish' } };
 
             astTypeExpectations(ast);
             expect(ast.body[0]).to.be.a('object').to.be.deep.equal(stmt);
@@ -292,22 +298,57 @@ describe('Compiler module', () => {
 
     it('should provide correct diagnostic reports', () => {
 
-        const report = createSyntaxScriptDiagnosticReport('TEST_FILE.syx', 'keyword ruleis');
+        function baseExpectations(rep: FullDocumentDiagnosticReport) {
+            expect(rep).to.be.a('object');
+            expect(rep).to.have.property('items').to.be.a('array');
+            expect(rep).to.has.property('kind').to.be.a('string').to.be.equal(DocumentDiagnosticReportKind.Full);
+        }
 
-        expect(report).to.be.a('object');
-        expect(report).to.have.property('items').to.be.a('array').to.have.lengthOf(1);
-        expect(report).to.have.property('kind').to.be.a('string').to.be.equal(DocumentDiagnosticReportKind.Full);
+        inst(() => {
 
-        const diag = report.items[0];
-        const item: Diagnostic = { message: 'Expected \';\' after statement, found \'EOF\'.', range: { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } }, severity: DiagnosticSeverity.Error, source: 'syntax-script', data: [] };
 
-        expect(diag).to.have.property('message').to.be.a('string');
-        expect(diag).to.have.property('range');
-        expect(diag).to.have.property('severity').to.be.a('number').to.be.equal(DiagnosticSeverity.Error);
-        rangeExpectations(diag.range);
-        expect(diag).to.have.property('source').to.be.a('string').to.be.equal('syntax-script');
-        expect(diag).to.be.a('object').to.be.deep.equal(item);
+            const report = createSyntaxScriptDiagnosticReport('TEST_FILE.syx', 'keyword ruleis');
 
-    });
+            baseExpectations(report);
+
+            const diag = report.items[0];
+            const item: Diagnostic = { message: 'Expected \';\' after statement, found \'EOF\'.', range: { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } }, severity: DiagnosticSeverity.Error, source: 'syntax-script', data: [] };
+
+            expect(diag).to.have.property('message').to.be.a('string');
+            expect(diag).to.have.property('range');
+            expect(diag).to.have.property('severity').to.be.a('number').to.be.equal(DiagnosticSeverity.Error);
+            rangeExpectations(diag.range);
+            expect(diag).to.have.property('source').to.be.a('string').to.be.equal('syntax-script');
+            expect(diag).to.be.a('object').to.be.deep.equal(item);
+
+        });
+
+        inst(() => {
+
+            const rep = createSyntaxScriptDiagnosticReport('TEST_FILE.syx', 'rule \'enforce-double-string-quotes\':false;rule \'enforce-single-string-quotes\':false;');
+
+            baseExpectations(rep);
+
+            const diag = rep.items[0];
+
+            expect(diag).to.be.deep.equal({
+                message: 'rule \'enforce-single-string-quotes\' conflicts with \'enforce-double-string-quotes\', both of them should not be defined.',
+                severity: DiagnosticSeverity.Error, range: r0(47, 77), source: 'syntax-script',data: [
+                    {
+                        title:'Remove enforce-double-string-quotes definition',
+                        kind:CodeActionKind.QuickFix,
+                        edit:{changes:{'TEST_FILE.syx':[{range:r0(0,42),newText:''}]}}
+                    },
+                    {
+                        title:'Remove enforce-single-string-quotes definition',
+                        kind:CodeActionKind.QuickFix,
+                        edit:{changes:{'TEST_FILE.syx':[{range:r0(42,83),newText:''}]}}
+                    }
+                ] as CodeAction[]
+            } as Diagnostic);
+
+        });
+
+    }, true);
 
 });
